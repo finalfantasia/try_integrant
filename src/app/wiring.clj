261b486e76c7c7aaafc3fff7cd576f507060f63d
Salam Elbilig <abdusalam@live.com>
2019-10-03 00:07:46 -0700

    switch the HTTP server to Jetty

--- src/app/wiring.clj
+++ src/app/wiring.clj
@@ -1,12 +1,10 @@
 (ns app.wiring
   (:require
-    [aleph.http :as aleph]
     [app.storage.now :as storage]
     [app.web.handlers :as handlers]
     [integrant.core :as ig]
-    [reitit.ring :as ring])
-  (:import
-    (java.io Closeable)))
+    [reitit.ring :as reitit]
+    [ring.adapter.jetty :as jetty]))
 
 
 (defmethod ig/init-key ::now-handler
@@ -28,28 +26,26 @@
 (defmethod ig/prep-key ::ring-handler
   [_ config]
   (merge config
-         (handlers/make-routes-options {:now-handler (ig/ref ::now-handler)
-                                        :ping-handler (ig/ref ::ping-handler)
-                                        :add-handler (ig/ref ::add-handler)})))
+         (handlers/make-routes-options
+           {:now-handler (ig/ref ::now-handler)
+            :ping-handler (ig/ref ::ping-handler)
+            :add-handler (ig/ref ::add-handler)})))
 
 (defmethod ig/init-key ::ring-handler
   [_ {:keys [routes options]}]
-  (ring/ring-handler (ring/router routes options)))
+  (reitit/ring-handler (reitit/router routes options)))
 
 (defmethod ig/init-key ::server
-  ^Closeable
-  [_ {:keys [port handler]}]
-  (aleph/start-server handler {:port port}))
+  [_ {:keys [handler] :as opts}]
+  (jetty/run-jetty handler (-> opts
+                               (dissoc :handler)
+                               (assoc :join? false))))
 
 (defmethod ig/halt-key! ::server
-  [_ ^Closeable server]
-  (.close server))
-
-;; if this is missing, Integrant falls back to defmethod for halt-key!
-;; (defmethod ig/suspend-key! ::instance
-;;   [_ ^Closeable server]
-;;   (.close server))
+  [_ server]
+  (.stop server))
 
 (defmethod ig/resume-key ::server
   [ig-key config _ _]
   (ig/init-key ig-key config))
+
